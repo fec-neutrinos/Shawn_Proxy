3a3f7f0383e522e543830a674b6248a285e22388
shawn-chambers <chambers.shawn.89@gmail.com>
2019-11-20 16:25:26 -0800

    Added seungjin service

--- server/proxy.js
+++ server/proxy.js
@@ -2,7 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const proxy = require('express-http-proxy');
+const morgan = require('morgan');
+const path = require('path');
 
+app.use(morgan('dev'));
 app.use(express.static('public'));
+
+// app.get('/:productName', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/public/index.html'));
+// })
 
 app.listen(port, () => console.log(`listening on port ${port}`));
